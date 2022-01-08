873abf0181c4d34b10112caf972f3df36dc50fac
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-01-08 09:06:16 +0000

    [test]

--- hello.rb
+++ hello.rb
@@ -1,2 +1,19 @@
 puts 'Hello,World!'
-ru
+def fizz_buzz(number)
+  if number % 15 == 0
+    "FizzBuss"
+  elsif number % 3 == 0
+    "Buzz"
+  elsif number % 5 ==0
+    "Fizz"
+  else
+    number.to_s
+  end
+end
+
+puts "数字を入力してください。"
+
+input = gets.to_i
+
+puts "結果は..."
+puts fizz_buzz(input)
